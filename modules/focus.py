a3bb8102062e5b43b5fd07fb2f1621402112e0cd
Tyler Knotek <tekkno89@gmail.com>
2022-01-30 20:51:23 -0800

    adds disable option to focus command

--- modules/focus.py
+++ modules/focus.py
@@ -12,6 +12,7 @@ from pprint import pprint
 home_dir = os.getenv('HOME')
 notify_config = home_dir + '/Library/Preferences/com.apple.ncprefs.plist'
 dock_config = home_dir + '/Library/Preferences/com.apple.dock.plist'
+pid_file = '/tmp/alfred_focus.pid'
 
 
 def get_plist_file(plist_file):
@@ -55,12 +56,29 @@ def hide_dock():
     subprocess.run(['osascript', '-e', 'tell application "System Events" to set autohide of dock preferences to not (autohide of dock preferences)'])
 
 
-def enable_focus():
+def enable_focus(length):
+    """Set Focus for X minutes"""
     set_dnd('on')
     hide_dock()
+    pid = os.fork()
+
+    if pid == 0:
+        focus_daemon(length)
+    elif pid > 0:
+        click.echo(f'Focus set for {str(int(length/60))} minute(s)')
+        track_focus_process(pid)
 
 
-def disable_focus():
+def disable_focus(terminate=False):
+    if terminate:
+        if os.path.exists(pid_file):
+            with open(pid_file, 'r') as file:
+                pid = file.read()
+            os.kill(int(pid), 9)
+            os.remove(pid_file)
+        else:
+            sys.exit('Focus does not appear to be running')
+
     set_dnd('off')
     hide_dock()
 
@@ -70,18 +88,20 @@ def focus_daemon(min):
     disable_focus()
 
 
-@click.command()
-@click.argument('length')
-def focus_command(length):
-    """Set Focus for X minutes"""
-    enable_focus()
-    min = int(length) * 60
-    pid = os.fork()
+def track_focus_process(pid):
+    with open(pid_file, 'w') as file:
+        file.write(str(pid))
 
-    if pid == 0:
-        focus_daemon(min)
-    elif pid > 0:
-        click.echo(f'Focus set for {str(length)} minute(s)')
+
+@click.command()
+@click.option('-e', '--enable', type=int)
+@click.option('-d', '--disable', is_flag=True)
+def focus_command(enable, disable):
+    if enable:
+        min = int(enable) * 60
+        enable_focus(min)
+    elif disable:
+        disable_focus(True)
 
 
 if __name__ == '__main__':
