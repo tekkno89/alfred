cdc1c507d54d8a642b82f2423b7acc8a7c493edd
Tyler Knotek <tekkno89@gmail.com>
2023-07-15 17:50:59 +0000

    updates focus daemon for osx13 and above

--- modules/focus.py
+++ modules/focus.py
@@ -1,73 +1,53 @@
-import plistlib
+#!/usr/bin/env python3
+
 import os
 import sys
 import subprocess
 import click
-import threading
 import time
-from datetime import datetime
+#from datetime import datetime
 
 home_dir = os.getenv('HOME')
-notify_config = home_dir + '/Library/Preferences/com.apple.ncprefs.plist'
-dock_config = home_dir + '/Library/Preferences/com.apple.dock.plist'
 pid_file = '/tmp/alfred_focus.pid'
+shortcut_name = 'macos-focus-mode'
 
 
-def get_plist_file(plist_file):
-    with open(plist_file, 'rb') as file:
-        settings_file = plistlib.load(file)
-
-    return settings_file
+# Make sure the short cut is installed, have to use this method for osx 13
+def check_shortcut_installed():
+    pass
 
 
-# Checks the NC plist to see if DND is enabled
-def check_dnd_stat(settings):
-    dnd_val = plistlib.loads(settings['dnd_prefs'])
-
-    return 'userPref' in dnd_val
-
-
-def set_dnd(status):
-    settings = get_plist_file(notify_config)
-    dnd_stat = check_dnd_stat(settings)
-    deserial_dnd = plistlib.loads(settings['dnd_prefs'])
-
-    if (status == 'on') & (dnd_stat is True):
-        sys.exit('DND is already enabled')
-    elif (status == 'off') & (dnd_stat is False):
-        sys.exit('DND is already disabled')
-    elif status == 'on':
-        user_pref_val = {'enabled': True, 'date': datetime.now(), 'reason': 1}
-        deserial_dnd['userPref'] = user_pref_val
-    elif status == 'off':
-        deserial_dnd.pop('userPref')
+def set_dnd(status, length=30):
+    if status == 'on':
+        shortcut_cmd = f'shortcuts run {shortcut_name} <<< "on {length}"'
     else:
-        sys.exit('Status not recognized')
+        shortcut_cmd = f'shortcuts run {shortcut_name} <<< "off"'
 
-    updated_dnd = plistlib.dumps(deserial_dnd, fmt=plistlib.FMT_BINARY)
-    subprocess.run(['/usr/bin/defaults', 'write', 'com.apple.ncprefs.plist', 'dnd_prefs', '-data', updated_dnd.hex()])
-    subprocess.run(['/usr/bin/killall', 'usernoted'])
+    subprocess.run(shortcut_cmd, shell=True)
 
 
 # Decided to use osascript because restarting dock will launch all the app windows you have minimized
-def hide_dock():
-    subprocess.run(['osascript', '-e', 'tell application "System Events" to set autohide of dock preferences to not (autohide of dock preferences)'])
+def toggle_dock():
+    subprocess.run(
+        ['osascript', '-e', 'tell application "System Events" to set autohide of dock preferences to not (autohide of dock preferences)']
+    )
 
 
 def enable_focus(length):
     """Set Focus for X minutes"""
-    set_dnd('on')
-    hide_dock()
+    set_dnd('on', length)
+    toggle_dock()
     pid = os.fork()
 
     if pid == 0:
         focus_daemon(length)
     elif pid > 0:
-        click.echo(f'Focus set for {str(int(length/60))} minute(s)')
+        click.echo(f'Focus set for {length} minute(s)')
         track_focus_process(pid)
 
 
 def disable_focus(terminate=False):
+    # This is for terminating focus before time is up
     if terminate:
         if os.path.exists(pid_file):
             with open(pid_file, 'r') as file:
@@ -78,11 +58,11 @@ def disable_focus(terminate=False):
             sys.exit('Focus does not appear to be running')
 
     set_dnd('off')
-    hide_dock()
+    toggle_dock()
 
 
 def focus_daemon(min):
-    time.sleep(min)
+    time.sleep(min * 60)
     disable_focus()
 
 
@@ -96,8 +76,8 @@ def track_focus_process(pid):
 @click.option('-d', '--disable', is_flag=True)
 def focus_command(enable, disable):
     if enable:
-        min = int(enable) * 60
-        enable_focus(min)
+        mins = enable
+        enable_focus(mins)
     elif disable:
         disable_focus(True)
 
